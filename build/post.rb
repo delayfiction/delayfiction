dfa111c3930ae1fbd1e8c3755e94917f77c4ef9c
Andrew Crimer <andrew.crimer@gmail.com>
2018-05-27 11:29:42 -0400

    puts files in folders now

--- build/post.rb
+++ build/post.rb
@@ -1,13 +1,27 @@
 require 'redcarpet'
+require 'yaml'
 
 class Post
-  attr_reader :content
+  attr_reader :content, :data
 
   def initialize(filename)
-    @content = File.read(filename).gsub(/\A---(.|\n)*?---/, '')
+    @_data = File.read(filename)
+    @content = @_data.gsub(/\A---(.|\n)*?---/, '')
   end
 
   def html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(@content)
+  end
+
+  def metadata
+    @metadata ||= YAML.load(@_data)
+  end
+
+  def issue
+    metadata['issue']
+  end
+
+  def issue_folder
+    Dir.pwd + "/" + issue + "/"
   end
 end
