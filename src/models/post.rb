b17c5fee4730495dab68a738828e8482fa9824c4
Andrew Crimer <andrew.crimer@gmail.com>
2018-11-10 12:47:23 -0500

    add post pagination; style and readability updates

--- src/models/post.rb
+++ src/models/post.rb
@@ -12,6 +12,23 @@ class Post < Base
     metadata.has_key? 'author'
   end
 
+  def this_issue
+    issue_id = metadata['issue']
+    Issue.find(File.join(@@source_dir, 'data', 'issues', (issue_id + '.md')))
+  end
+
+  def next_post
+    next_post_ordinal = issue_order
+    next_post = this_issue.stories[next_post_ordinal]
+  end
+
+  def prev_post
+    prev_post_ordinal = issue_order - 2
+    if prev_post_ordinal >= 0
+      prev_post = this_issue.stories[prev_post_ordinal]
+    end
+  end
+
   def render(name)
     @post = self
     template = File.read(File.join(@@source_dir, "templates", (name + '.html.erb')))
